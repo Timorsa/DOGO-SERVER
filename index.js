d167328dcf9fdf947ae8ac33b5295af905b41458
Timor Safadi <48487764+Timorsa@users.noreply.github.com>
2020-02-19 19:22:05 +0200

    some logic left

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const path = require('path');
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./src/db/dbConnection');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+
+// Routes
+const petRouter = require('./src/routes/pet');
+const userRouter = require('./src/routes/user');
+const feedRouter = require('./src/routes/feed');
+const eventRouter = require('./src/routes/event');
+
+
+app.use('/pet', petRouter);
+app.use('/user', userRouter);
+app.use('/feed', feedRouter);
+app.use('/event', eventRouter);
+
+app.use((req, res, next) => {
+  let err = new Error('Not Found !!');
+  err.status = 404;
+  next(err);
+});
+
+const errorHandler = require('./src/handlers/error');
+app.use(errorHandler);
+
+const server = app.listen(PORT, () => {
+  console.log(`Server: server is ready for requests on : ${PORT}`);
+});
